0aa7850773fe4e1c34a302113818d9e1fe1301fd
Ray Kootstra <r.kootstra@kootstradevelopment.nl>
2017-06-01 19:16:50 +0200

    Changed symlink to cp statement

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p ./web
-ln -s ./vendor/stackinstance/docker-centos/docker ./
-ln -s ./vendor/stackinstance/docker-centos/docker-compose.yml ./
+cp -R ./vendor/stackinstance/docker-centos/docker ./
+cp -R ./vendor/stackinstance/docker-centos/docker-compose.yml ./
