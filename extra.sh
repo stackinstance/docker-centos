858ef4ff128ef242ca8ef1d15b102cc1ba8a93e4
Stack Instance <ray@kootstra.net>
2017-08-14 11:51:39 +0200

    commented example for extr install when docker image is created

---
+++ extra.sh
@@ -0,0 +1,8 @@
+# Add nodejs
+#RUN curl -sL https://rpm.nodesource.com/setup_6.x | bash -
+#RUN yum install -y nodejs
+#RUN npm install npm grunt
+
+# Add bundler
+#RUN yum -y update && yum -y install -y ruby ruby-dev make
+#RUN gem install bundler
