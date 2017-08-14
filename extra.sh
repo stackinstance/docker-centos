b8830730575427c1b3294f031e8f337ae0fbbb17
Stack Instance <ray@kootstra.net>
2017-08-14 15:29:28 +0200

    Update extra.sh

--- extra.sh
+++ extra.sh
@@ -1,8 +1,8 @@
 # Add nodejs
-#RUN curl -sL https://rpm.nodesource.com/setup_6.x | bash -
-#RUN yum install -y nodejs
-#RUN npm install npm grunt
+#curl -sL https://rpm.nodesource.com/setup_6.x | bash -
+#yum install -y nodejs
+#npm install npm grunt
 
 # Add bundler
-#RUN yum -y update && yum -y install -y ruby ruby-dev make
-#RUN gem install bundler
+#yum -y update && yum -y install -y ruby ruby-dev make
+#gem install bundler
