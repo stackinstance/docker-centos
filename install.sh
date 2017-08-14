2daf3dacffa4cfc8cfc27f8eb50e7812f3f530bd
Stack Instance <ray@kootstra.net>
2017-08-14 14:36:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@
 mkdir -p ./web
 ln -s ./vendor/stackinstance/docker-centos/docker ./
 cp -R ./vendor/stackinstance/docker-centos/docker-compose.yml ./
-cp -R ./vendor/stackinstance/docker-centos/extra.sh ./
 cp -R ./vendor/stackinstance/docker-centos/project.cfg ./
-cp -R ./vendor/stackinstance/docker-centos/extra.sh ./
+if [ ! -f ./extra.sh ]; then cp -R ./vendor/stackinstance/docker-centos/extra.sh ./; fi
