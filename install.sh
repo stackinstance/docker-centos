47008b3335f1301490fcb4bbf6fc1900b120791f
Stack Instance <ray@kootstra.net>
2017-08-14 13:19:54 +0200

    Added extra bash script to install in docker image

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s ./vendor/stackinstance/docker-centos/docker ./
 cp -R ./vendor/stackinstance/docker-centos/docker-compose.yml ./
 cp -R ./vendor/stackinstance/docker-centos/extra.sh ./
 cp -R ./vendor/stackinstance/docker-centos/project.cfg ./
+cp -R ./vendor/stackinstance/docker-centos/extra.sh ./
