527d7903b3d8934daffa165a670e74cf26b7c648
Stack Instance <ray@kootstra.net>
2017-08-14 11:59:07 +0200

    Update rebuild.sh

--- docker/tools/rebuild.sh
+++ docker/tools/rebuild.sh
@@ -2,6 +2,14 @@
 php ../../composer.phar update stackinstance/docker-centos
 bash ../../vendor/stackinstance/docker-centos/install.sh
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$DIR/bootstrap.sh"
+
+read -p "Project '$PROJECTNAME' will be rebuild, the db will be deleted, type 'yes' to continue "
+if [ "$REPLY" != "yes" ]; then
+   exit
+fi
+
 docker-compose --project-name project down
 docker rmi project_php-apache
 docker-compose --project-name project build --no-cache
