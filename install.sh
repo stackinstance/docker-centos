1a9ea758b0247782ae3cba4c302ca5815e7a9ff9
Stack Instance <ray@kootstra.net>
2017-08-14 11:53:26 +0200

    Added extra install script

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 mkdir -p ./web
 ln -s ./vendor/stackinstance/docker-centos/docker ./
 cp -R ./vendor/stackinstance/docker-centos/docker-compose.yml ./
+cp -R ./vendor/stackinstance/docker-centos/install.sh ./
